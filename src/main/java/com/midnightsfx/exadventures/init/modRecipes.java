1fbc870be378ce64d5bb97793caa4c78d5592ab3
MidnightsFX <carl.stutz@iovation.com>
2017-06-16 09:59:01 -0700

    Updating COFH stuff and Fixing some thermal foundation tool/weapon smeltings

--- src/main/java/com/midnightsfx/exadventures/init/modRecipes.java
+++ src/main/java/com/midnightsfx/exadventures/init/modRecipes.java
@@ -77,13 +77,13 @@ public class modRecipes {
             TinkerRegistry.registerMelting(TFEquipment.ArmorSet.COPPER.armorLegs, TinkerFluids.copper, Material.VALUE_Ingot * 3);
             TinkerRegistry.registerMelting(TFEquipment.ArmorSet.COPPER.armorChestplate, TinkerFluids.copper, Material.VALUE_Ingot * 4);
             // Tools
-            TinkerRegistry.registerMelting(TFEquipment.ToolSet.COPPER.itemPickaxe, TinkerFluids.bronze, Material.VALUE_Ingot * 2 );
-            TinkerRegistry.registerMelting(TFEquipment.ToolSet.COPPER.itemAxe, TinkerFluids.bronze, Material.VALUE_Ingot * 2 );
-            TinkerRegistry.registerMelting(TFEquipment.ToolSet.COPPER.itemHoe, TinkerFluids.bronze, Material.VALUE_Ingot );
-            TinkerRegistry.registerMelting(TFEquipment.ToolSet.COPPER.itemShovel, TinkerFluids.bronze, Material.VALUE_Ingot );
-            TinkerRegistry.registerMelting(TFEquipment.ToolSet.COPPER.itemFishingRod, TinkerFluids.bronze, Material.VALUE_Ingot );
-            TinkerRegistry.registerMelting(TFEquipment.ToolSet.COPPER.itemSickle, TinkerFluids.bronze, Material.VALUE_Ingot * 2 );
-            TinkerRegistry.registerMelting(TFEquipment.ToolSet.COPPER.itemShears, TinkerFluids.bronze, Material.VALUE_Ingot );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.COPPER.itemPickaxe, TinkerFluids.copper, Material.VALUE_Ingot * 2 );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.COPPER.itemAxe, TinkerFluids.copper, Material.VALUE_Ingot * 2 );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.COPPER.itemHoe, TinkerFluids.copper, Material.VALUE_Ingot );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.COPPER.itemShovel, TinkerFluids.copper, Material.VALUE_Ingot );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.COPPER.itemFishingRod, TinkerFluids.copper, Material.VALUE_Ingot );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.COPPER.itemSickle, TinkerFluids.copper, Material.VALUE_Ingot * 2 );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.COPPER.itemShears, TinkerFluids.copper, Material.VALUE_Ingot );
             // Weapons
             TinkerRegistry.registerMelting(TFEquipment.ToolSet.COPPER.itemBow, TinkerFluids.bronze, Material.VALUE_Ingot * 2 );
             TinkerRegistry.registerMelting(TFEquipment.ToolSet.COPPER.itemSword, TinkerFluids.bronze, Material.VALUE_Ingot );
@@ -95,30 +95,95 @@ public class modRecipes {
             TinkerRegistry.registerMelting(TFEquipment.ArmorSet.TIN.armorHelmet, TinkerFluids.tin, Material.VALUE_Ingot * 2);
             TinkerRegistry.registerMelting(TFEquipment.ArmorSet.TIN.armorLegs, TinkerFluids.tin, Material.VALUE_Ingot * 3);
             TinkerRegistry.registerMelting(TFEquipment.ArmorSet.TIN.armorChestplate, TinkerFluids.tin, Material.VALUE_Ingot * 4);
+            // Tools
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.TIN.itemPickaxe, TinkerFluids.tin, Material.VALUE_Ingot * 2 );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.TIN.itemAxe, TinkerFluids.tin, Material.VALUE_Ingot * 2 );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.TIN.itemHoe, TinkerFluids.tin, Material.VALUE_Ingot );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.TIN.itemShovel, TinkerFluids.tin, Material.VALUE_Ingot );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.TIN.itemFishingRod, TinkerFluids.tin, Material.VALUE_Ingot );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.TIN.itemSickle, TinkerFluids.tin, Material.VALUE_Ingot * 2 );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.TIN.itemShears, TinkerFluids.tin, Material.VALUE_Ingot );
+            // Weapons
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.TIN.itemBow, TinkerFluids.tin, Material.VALUE_Ingot * 2 );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.TIN.itemSword, TinkerFluids.tin, Material.VALUE_Ingot );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.TIN.itemShield, TinkerFluids.tin, Material.VALUE_Ingot * 2 );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.TIN.itemHammer, TinkerFluids.tin, Material.VALUE_Ingot * 2 );
 
             ///////// Melt Silver /////////
             TinkerRegistry.registerMelting(TFEquipment.ArmorSet.SILVER.armorBoots, TinkerFluids.silver, Material.VALUE_Ingot * 2);
             TinkerRegistry.registerMelting(TFEquipment.ArmorSet.SILVER.armorHelmet, TinkerFluids.silver, Material.VALUE_Ingot * 2);
             TinkerRegistry.registerMelting(TFEquipment.ArmorSet.SILVER.armorLegs, TinkerFluids.silver, Material.VALUE_Ingot * 3);
             TinkerRegistry.registerMelting(TFEquipment.ArmorSet.SILVER.armorChestplate, TinkerFluids.silver, Material.VALUE_Ingot * 4);
+            // Tools
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.SILVER.itemPickaxe, TinkerFluids.silver, Material.VALUE_Ingot * 2 );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.SILVER.itemAxe, TinkerFluids.silver, Material.VALUE_Ingot * 2 );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.SILVER.itemHoe, TinkerFluids.silver, Material.VALUE_Ingot );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.SILVER.itemShovel, TinkerFluids.silver, Material.VALUE_Ingot );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.SILVER.itemFishingRod, TinkerFluids.silver, Material.VALUE_Ingot );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.SILVER.itemSickle, TinkerFluids.silver, Material.VALUE_Ingot * 2 );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.SILVER.itemShears, TinkerFluids.silver, Material.VALUE_Ingot );
+            // Weapons
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.SILVER.itemBow, TinkerFluids.silver, Material.VALUE_Ingot * 2 );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.SILVER.itemSword, TinkerFluids.silver, Material.VALUE_Ingot );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.SILVER.itemShield, TinkerFluids.silver, Material.VALUE_Ingot * 2 );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.SILVER.itemHammer, TinkerFluids.silver, Material.VALUE_Ingot * 2 );
 
             ///////// Melt Lead /////////
             TinkerRegistry.registerMelting(TFEquipment.ArmorSet.LEAD.armorBoots, TinkerFluids.lead, Material.VALUE_Ingot * 2);
             TinkerRegistry.registerMelting(TFEquipment.ArmorSet.LEAD.armorHelmet, TinkerFluids.lead, Material.VALUE_Ingot * 2);
             TinkerRegistry.registerMelting(TFEquipment.ArmorSet.LEAD.armorLegs, TinkerFluids.lead, Material.VALUE_Ingot * 3);
             TinkerRegistry.registerMelting(TFEquipment.ArmorSet.LEAD.armorChestplate, TinkerFluids.lead, Material.VALUE_Ingot * 4);
+            // Tools
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.LEAD.itemPickaxe, TinkerFluids.lead, Material.VALUE_Ingot * 2 );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.LEAD.itemAxe, TinkerFluids.lead, Material.VALUE_Ingot * 2 );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.LEAD.itemHoe, TinkerFluids.lead, Material.VALUE_Ingot );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.LEAD.itemShovel, TinkerFluids.lead, Material.VALUE_Ingot );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.LEAD.itemFishingRod, TinkerFluids.lead, Material.VALUE_Ingot );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.LEAD.itemSickle, TinkerFluids.lead, Material.VALUE_Ingot * 2 );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.LEAD.itemShears, TinkerFluids.lead, Material.VALUE_Ingot );
+            // Weapons
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.LEAD.itemBow, TinkerFluids.lead, Material.VALUE_Ingot * 2 );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.LEAD.itemSword, TinkerFluids.lead, Material.VALUE_Ingot );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.LEAD.itemShield, TinkerFluids.lead, Material.VALUE_Ingot * 2 );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.LEAD.itemHammer, TinkerFluids.lead, Material.VALUE_Ingot * 2 );
 
             ///////// Melt Nickel /////////
             TinkerRegistry.registerMelting(TFEquipment.ArmorSet.NICKEL.armorBoots, TinkerFluids.nickel, Material.VALUE_Ingot * 2);
             TinkerRegistry.registerMelting(TFEquipment.ArmorSet.NICKEL.armorHelmet, TinkerFluids.nickel, Material.VALUE_Ingot * 2);
             TinkerRegistry.registerMelting(TFEquipment.ArmorSet.NICKEL.armorLegs, TinkerFluids.nickel, Material.VALUE_Ingot * 3);
             TinkerRegistry.registerMelting(TFEquipment.ArmorSet.NICKEL.armorChestplate, TinkerFluids.nickel, Material.VALUE_Ingot * 4);
+            // Tools
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.NICKEL.itemPickaxe, TinkerFluids.nickel, Material.VALUE_Ingot * 2 );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.NICKEL.itemAxe, TinkerFluids.nickel, Material.VALUE_Ingot * 2 );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.NICKEL.itemHoe, TinkerFluids.nickel, Material.VALUE_Ingot );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.NICKEL.itemShovel, TinkerFluids.nickel, Material.VALUE_Ingot );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.NICKEL.itemFishingRod, TinkerFluids.nickel, Material.VALUE_Ingot );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.NICKEL.itemSickle, TinkerFluids.nickel, Material.VALUE_Ingot * 2 );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.NICKEL.itemShears, TinkerFluids.nickel, Material.VALUE_Ingot );
+            // Weapons
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.NICKEL.itemBow, TinkerFluids.nickel, Material.VALUE_Ingot * 2 );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.NICKEL.itemSword, TinkerFluids.nickel, Material.VALUE_Ingot );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.NICKEL.itemShield, TinkerFluids.nickel, Material.VALUE_Ingot * 2 );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.NICKEL.itemHammer, TinkerFluids.nickel, Material.VALUE_Ingot * 2 );
 
             ///////// Melt Electrum /////////
             TinkerRegistry.registerMelting(TFEquipment.ArmorSet.ELECTRUM.armorBoots, TinkerFluids.electrum, Material.VALUE_Ingot * 2);
             TinkerRegistry.registerMelting(TFEquipment.ArmorSet.ELECTRUM.armorHelmet, TinkerFluids.electrum, Material.VALUE_Ingot * 2);
             TinkerRegistry.registerMelting(TFEquipment.ArmorSet.ELECTRUM.armorLegs, TinkerFluids.electrum, Material.VALUE_Ingot * 3);
             TinkerRegistry.registerMelting(TFEquipment.ArmorSet.ELECTRUM.armorChestplate, TinkerFluids.electrum, Material.VALUE_Ingot * 4);
+            // Tools
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.ELECTRUM.itemPickaxe, TinkerFluids.electrum, Material.VALUE_Ingot * 2 );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.ELECTRUM.itemAxe, TinkerFluids.electrum, Material.VALUE_Ingot * 2 );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.ELECTRUM.itemHoe, TinkerFluids.electrum, Material.VALUE_Ingot );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.ELECTRUM.itemShovel, TinkerFluids.electrum, Material.VALUE_Ingot );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.ELECTRUM.itemFishingRod, TinkerFluids.electrum, Material.VALUE_Ingot );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.ELECTRUM.itemSickle, TinkerFluids.electrum, Material.VALUE_Ingot * 2 );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.ELECTRUM.itemShears, TinkerFluids.electrum, Material.VALUE_Ingot );
+            // Weapons
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.ELECTRUM.itemBow, TinkerFluids.electrum, Material.VALUE_Ingot * 2 );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.ELECTRUM.itemSword, TinkerFluids.electrum, Material.VALUE_Ingot );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.ELECTRUM.itemShield, TinkerFluids.electrum, Material.VALUE_Ingot * 2 );
+            TinkerRegistry.registerMelting(TFEquipment.ToolSet.ELECTRUM.itemHammer, TinkerFluids.electrum, Material.VALUE_Ingot * 2 );
 
             ///////// Melt Invar /////////
             // - This currently does not have an ingot it turns into in the smeltery
